f60f2640d6eaa78f71771ce0ef695b2fa3dbf13d
JesseSquires181 <jessesquires181@gmail.com>
2021-12-03 14:57:47 -0700

    DB change

--- user.py
+++ user.py
@@ -38,5 +38,5 @@ class User(UserMixin):
             "VALUES (%s, %s, %s, %s, %s)",
             (id_, name, email, profile_pic, role),
         )
-        cursor.commit()
+        db.commit()
         cursor.close()
